846c52b4fb8ac2a72cb011161e907c0a39f15935
farbod-behnam <farbod.behnam@gmail.com>
2022-07-10 18:02:49 +0430

    feat[payment]: add implementation of IPaymentService interface

---
+++ src/main/java/com/PaymentApplication/service/impl/PaymentService.java
@@ -0,0 +1,59 @@
+package com.PaymentApplication.service.impl;
+
+import com.PaymentApplication.dto.request.payment.PaymentOrderRequest;
+import com.PaymentApplication.entity.AppUser;
+import com.PaymentApplication.entity.Transaction;
+import com.PaymentApplication.entity.Wallet;
+import com.PaymentApplication.enums.TransactionStatusEnum;
+import com.PaymentApplication.exception.NotFoundException;
+import com.PaymentApplication.service.IPaymentService;
+import com.PaymentApplication.service.ITransactionService;
+import com.PaymentApplication.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService implements IPaymentService
+{
+    private final IUserService userService;
+    private final ITransactionService transactionService;
+
+    @Autowired
+    public PaymentService(IUserService userService, ITransactionService transactionService)
+    {
+        this.userService = userService;
+        this.transactionService = transactionService;
+    }
+
+
+    @Override
+    public void chargeCard(PaymentOrderRequest paymentOrderRequest)
+    {
+        AppUser foundUser = userService.findUserByUsername(paymentOrderRequest.getUsername());
+        Wallet userWallet = foundUser.getWallet();
+
+        if (userWallet == null)
+            throw new NotFoundException("User does not have a wallet yet");
+
+        // if user does not have enough credit for purchasing the order
+        if (userWallet.getCredit().compareTo(paymentOrderRequest.getTotalPrice()) < 0)
+        {
+            paymentOrderRequest.setTransactionStatus(TransactionStatusEnum.NOT_ENOUGH_CREDITS.name());
+            Transaction transaction = transactionService.createTransaction(paymentOrderRequest);
+            foundUser.addTransaction(transaction);
+
+            // TODO send paymentOrderRequest back to online shop application
+        }
+        else
+        {
+            userWallet.subtractFee(paymentOrderRequest.getTotalPrice());
+            paymentOrderRequest.setTransactionStatus(TransactionStatusEnum.PURCHASED.name());
+            Transaction transaction = transactionService.createTransaction(paymentOrderRequest);
+            foundUser.addTransaction(transaction);
+
+
+            // TODO send paymentOrderRequest back to online shop application
+        }
+
+    }
+}
