c83f2cf02479ef5ad8e300de372e88c1c730afc6
farbod-behnam <farbod.behnam@gmail.com>
2022-07-11 10:25:39 +0430

    feat[RabbitMQ]: add config for payment application exchange, queue order and routing key

--- src/main/java/com/PaymentApplication/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/PaymentApplication/rabbitmq/RabbitMQConfig.java
@@ -13,6 +13,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig
 {
 
+    // #############################
+    // ONLINE SHOP STRINGS
+    // #############################
+
     @Value("${onlineshop.app.rabbitmq.exchange}")
     private String ONLINE_SHOP_EXCHANGE;
 
@@ -20,13 +24,30 @@ public class RabbitMQConfig
     private String ONLINE_SHOP_ORDER_QUEUE;
 
     @Value("${onlineshop.app.rabbitmq.routingKey.order}")
-    private String ORDER_ROUTING_KEY;
+    private String ONLINE_SHOP_ORDER_ROUTING_KEY;
 
     @Value("${onlineshop.app.rabbitmq.queue.user}")
     private String ONLINE_SHOP_USER_QUEUE;
 
     @Value("${onlineshop.app.rabbitmq.routingKey.user}")
-    private String USER_ROUTING_KEY;
+    private String ONLINE_SHOP_USER_ROUTING_KEY;
+
+    // #############################
+    // PAYMENT APPLICATION STRINGS
+    // #############################
+
+    @Value("${payment.app.rabbitmq.exchange}")
+    private String PAYMENT_APP_EXCHANGE;
+
+    @Value("${payment.app.rabbitmq.queue.order}")
+    private String PAYMENT_APP_ORDER_QUEUE;
+
+    @Value("${payment.app.rabbitmq.routingKey.order}")
+    private String PAYMENT_APP_ORDER_ROUTING_KEY;
+
+    // #############################
+    // ONLINE SHOP
+    // #############################
 
     @Bean
     public TopicExchange onlineShopExchange()
@@ -49,14 +70,37 @@ public class RabbitMQConfig
     @Bean
     public Binding onlineShopOrderBinding(Queue onlineShopOrderQueue, TopicExchange onlineShopExchange)
     {
-        return BindingBuilder.bind(onlineShopOrderQueue).to(onlineShopExchange).with(ORDER_ROUTING_KEY);
+        return BindingBuilder.bind(onlineShopOrderQueue).to(onlineShopExchange).with(ONLINE_SHOP_ORDER_ROUTING_KEY);
     }
 
     @Bean
     public Binding onlineShopUserBinding(Queue onlineShopUserQueue, TopicExchange onlineShopExchange)
     {
-        return BindingBuilder.bind(onlineShopUserQueue).to(onlineShopExchange).with(USER_ROUTING_KEY);
+        return BindingBuilder.bind(onlineShopUserQueue).to(onlineShopExchange).with(ONLINE_SHOP_USER_ROUTING_KEY);
     }
+
+    // #############################
+    // PAYMENT APPLICATION
+    // #############################
+
+    @Bean
+    public TopicExchange paymentAppExchange()
+    {
+        return new TopicExchange(PAYMENT_APP_EXCHANGE);
+    }
+
+    @Bean
+    public Queue paymentAppOrderQueue()
+    {
+        return new Queue(PAYMENT_APP_ORDER_QUEUE);
+    }
+
+    @Bean
+    public Binding paymentAppOrderBinding(Queue paymentAppOrderQueue, TopicExchange paymentAppExchange)
+    {
+        return BindingBuilder.bind(paymentAppOrderQueue).to(paymentAppExchange).with(PAYMENT_APP_ORDER_ROUTING_KEY);
+    }
+
 
     /**
      * since we use object to send message to rabbitMQ, therefore
